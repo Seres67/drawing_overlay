0ecf81c8823b141cbd7d20f95589c597996a91f1
Laurent <seres@seres.eu.org>
2025-06-09 01:19:42 +0200

    undo/redo

--- include/app.h
+++ include/app.h
@@ -1,16 +1,27 @@
 #ifndef DRAWING_OVERLAY_APP_H_
 #define DRAWING_OVERLAY_APP_H_
 
+#include <SDL3/SDL_rect.h>
 #include <SDL3/SDL_render.h>
 #include <SDL3_ttf/SDL_ttf.h>
 #include <mouse.h>
+#include <stdbool.h>
+
+#define POINTS_COUNT 2500
+#define STROKES_COUNT 200
 
 typedef struct
 {
-
     int count;
-    SDL_FRect lines[100000];
-} Lines;
+    SDL_FPoint points[POINTS_COUNT];
+    bool hidden;
+} Stroke;
+
+typedef struct
+{
+    int count;
+    Stroke strokes[STROKES_COUNT];
+} Strokes;
 
 typedef struct
 {
@@ -18,8 +29,15 @@ typedef struct
     SDL_Renderer *renderer;
     bool running;
     MouseState mouse_state;
-    Lines lines;
+    Strokes strokes;
     TTF_Font *font;
 } App;
+
+// TODO: changes lines to strokes
+// each stroke is an array of rects
+// store array of strokes
+// when ctrl+z hide last troke
+// if drawing while last stroke is hidden, replace it
+// ctrl+y unhide last stroke
 
 #endif // DRAWING_OVERLAY_APP_H_
--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include "button.h"
 #include "mouse.h"
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_events.h>
+#include <SDL3/SDL_rect.h>
+#include <SDL3/SDL_render.h>
 #include <app.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -8,14 +11,16 @@
 
 void reset_screen(App *app)
 {
-    memset(&app->lines.lines, 0, 100000 * sizeof(SDL_FRect));
-    app->lines.count = 0;
+    for (int i = 0; i < app->strokes.count; ++i)
+        memset(app->strokes.strokes[i].points, 0, POINTS_COUNT * sizeof(SDL_FRect));
+    memset(&app->strokes.strokes, 0, STROKES_COUNT * sizeof(Stroke));
+    app->strokes.count = 0;
 };
 
 int main(int argc, char *argv[])
 {
-    App app = {NULL, NULL, true, {false, false, 0, 0}, {0}};
-    Button reset = {"Clear", {100, 100, 40, 40}, NULL, reset_screen};
+    App app = {NULL, NULL, true, {false, false, 0, 0}, {0}, NULL};
+    Button clear = {"Clear", {100, 100, 40, 40}, NULL, reset_screen};
 
     if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS)) {
         SDL_Log("couldn't init SDL: %s\n", SDL_GetError());
@@ -40,12 +45,12 @@ int main(int argc, char *argv[])
     }
 
     SDL_Color color = {255, 255, 255, SDL_ALPHA_OPAQUE};
-    SDL_Surface *text = TTF_RenderText_Blended(app.font, reset.text, 5, color);
+    SDL_Surface *text = TTF_RenderText_Blended(app.font, clear.text, 5, color);
     if (text) {
-        reset.texture = SDL_CreateTextureFromSurface(app.renderer, text);
+        clear.texture = SDL_CreateTextureFromSurface(app.renderer, text);
         SDL_DestroySurface(text);
     }
-    if (!reset.texture) {
+    if (!clear.texture) {
         SDL_Log("couldn't create texture: %s\n", SDL_GetError());
         return SDL_APP_FAILURE;
     }
@@ -57,27 +62,49 @@ int main(int argc, char *argv[])
                 app.running = false;
             if (event.type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
                 if (event.button.button == MouseLeft) {
-                    if (is_button_clicked(reset, event.button.x, event.button.y)) {
-                        reset.callback(&app);
+                    if (is_button_clicked(clear, event.button.x, event.button.y)) {
+                        clear.callback(&app);
                     }
                     app.mouse_state.mouse_left = true;
                     app.mouse_state.x = event.motion.x;
                     app.mouse_state.y = event.motion.y;
+                    if (app.strokes.strokes[app.strokes.count].hidden) {
+                        app.strokes.strokes[app.strokes.count].hidden = false;
+                        memset(app.strokes.strokes[app.strokes.count].points, 0, POINTS_COUNT * sizeof(SDL_FPoint));
+                        app.strokes.strokes[app.strokes.count].count = 0;
+                        // TODO: also memset 0 all following strokes and points and reset count and hidden
+                    }
+                    app.strokes.count++;
                 }
                 if (event.button.button == MouseRight)
                     app.mouse_state.mouse_right = true;
             }
             if (event.type == SDL_EVENT_MOUSE_BUTTON_UP) {
-                if (event.button.button == MouseLeft)
+                if (event.button.button == MouseLeft) {
                     app.mouse_state.mouse_left = false;
+                }
                 if (event.button.button == MouseRight)
                     app.mouse_state.mouse_right = false;
             }
+            if (!app.mouse_state.mouse_left && event.type == SDL_EVENT_KEY_DOWN) {
+                if (event.key.mod == SDL_KMOD_LCTRL) {
+                    if (event.key.key == SDLK_Z) {
+                        if (app.strokes.count > 0) {
+                            app.strokes.strokes[app.strokes.count - 1].hidden = true;
+                            app.strokes.count--;
+                        }
+                    } else if (event.key.key == SDLK_Y) {
+                        if (app.strokes.strokes[app.strokes.count].hidden) {
+                            app.strokes.strokes[app.strokes.count].hidden = false;
+                            app.strokes.count++;
+                        }
+                    }
+                }
+            }
             if (app.mouse_state.mouse_left && event.type == SDL_EVENT_MOUSE_MOTION) {
-                app.lines.lines[app.lines.count] =
-                    (SDL_FRect){app.mouse_state.x, app.mouse_state.y, event.motion.xrel == 0 ? 1 : event.motion.xrel,
-                                event.motion.yrel == 0 ? 1 : event.motion.yrel};
-                app.lines.count++;
+                app.strokes.strokes[app.strokes.count - 1].points[app.strokes.strokes[app.strokes.count - 1].count] =
+                    (SDL_FPoint){app.mouse_state.x, app.mouse_state.y};
+                app.strokes.strokes[app.strokes.count - 1].count++;
                 app.mouse_state.x += event.motion.xrel;
                 app.mouse_state.y += event.motion.yrel;
             }
@@ -88,15 +115,15 @@ int main(int argc, char *argv[])
         // TODO: draw corners
 
         SDL_SetRenderDrawColor(app.renderer, 0, 0, 0, 255);
-        SDL_RenderFillRect(app.renderer, &reset.rect);
+        SDL_RenderFillRect(app.renderer, &clear.rect);
 
         SDL_SetRenderDrawColor(app.renderer, 255, 0, 0, 255);
 
-        SDL_RenderTexture(app.renderer, reset.texture, NULL, &reset.rect);
+        SDL_RenderTexture(app.renderer, clear.texture, NULL, &clear.rect);
 
-        for (int i = 0; i < app.lines.count; ++i)
-            SDL_RenderFillRect(app.renderer, &app.lines.lines[i]);
-
+        for (int i = 0; i < app.strokes.count; ++i)
+            if (!app.strokes.strokes[i].hidden)
+                SDL_RenderLines(app.renderer, app.strokes.strokes[i].points, app.strokes.strokes[i].count);
         SDL_RenderPresent(app.renderer);
     }
     return 0;
