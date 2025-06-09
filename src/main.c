28a4375da96e294146038c9264f746e5410b49de
Laurent <seres@seres.eu.org>
2025-06-09 02:13:50 +0200

    added undo, redo, save buttons. save is stub for now

--- src/main.c
+++ src/main.c
@@ -17,10 +17,32 @@ void reset_screen(App *app)
     app->strokes.count = 0;
 };
 
+void undo_stroke(App *app)
+{
+    if (app->strokes.count > 0) {
+        app->strokes.strokes[app->strokes.count - 1].hidden = true;
+        app->strokes.count--;
+    }
+}
+
+void redo_stroke(App *app)
+{
+    if (app->strokes.strokes[app->strokes.count].hidden) {
+        app->strokes.strokes[app->strokes.count].hidden = false;
+        app->strokes.count++;
+    }
+}
+
+void save_to_file(App *app) {}
+
 int main(int argc, char *argv[])
 {
     App app = {NULL, NULL, true, {false, false, 0, 0}, {0}, NULL};
-    Button clear = {"Clear", {100, 100, 40, 40}, NULL, reset_screen};
+    Button clear = {"Clear", {0, 0, 40, 40}, NULL, reset_screen};
+    Button undo = {"Undo", {40, 0, 40, 40}, NULL, undo_stroke};
+    Button redo = {"Redo", {80, 0, 40, 40}, NULL, redo_stroke};
+    Button save = {"Save", {120, 0, 40, 40}, NULL, save_to_file};
+    Button buttons[] = {clear, undo, redo, save, {NULL}};
 
     if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS)) {
         SDL_Log("couldn't init SDL: %s\n", SDL_GetError());
@@ -45,14 +67,16 @@ int main(int argc, char *argv[])
     }
 
     SDL_Color color = {255, 255, 255, SDL_ALPHA_OPAQUE};
-    SDL_Surface *text = TTF_RenderText_Blended(app.font, clear.text, 5, color);
-    if (text) {
-        clear.texture = SDL_CreateTextureFromSurface(app.renderer, text);
-        SDL_DestroySurface(text);
-    }
-    if (!clear.texture) {
-        SDL_Log("couldn't create texture: %s\n", SDL_GetError());
-        return SDL_APP_FAILURE;
+    for (int i = 0; buttons[i].text; ++i) {
+        SDL_Surface *text = TTF_RenderText_Blended(app.font, buttons[i].text, 5, color);
+        if (text) {
+            buttons[i].texture = SDL_CreateTextureFromSurface(app.renderer, text);
+            SDL_DestroySurface(text);
+        }
+        if (!buttons[i].texture) {
+            SDL_Log("couldn't create texture: %s\n", SDL_GetError());
+            return SDL_APP_FAILURE;
+        }
     }
 
     SDL_Event event;
@@ -62,10 +86,16 @@ int main(int argc, char *argv[])
                 app.running = false;
             if (event.type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
                 if (event.button.button == MouseLeft) {
-                    if (is_button_clicked(clear, event.button.x, event.button.y)) {
-                        clear.callback(&app);
-                    }
                     app.mouse_state.mouse_left = true;
+                    bool found_button = false;
+                    for (int i = 0; buttons[i].text; ++i) {
+                        if (is_button_clicked(buttons[i], event.button.x, event.button.y)) {
+                            buttons[i].callback(&app);
+                            found_button = true;
+                        }
+                    }
+                    if (found_button)
+                        break;
                     app.mouse_state.x = event.motion.x;
                     app.mouse_state.y = event.motion.y;
                     if (app.strokes.strokes[app.strokes.count].hidden) {
@@ -89,15 +119,9 @@ int main(int argc, char *argv[])
             if (!app.mouse_state.mouse_left && event.type == SDL_EVENT_KEY_DOWN) {
                 if (event.key.mod == SDL_KMOD_LCTRL) {
                     if (event.key.key == SDLK_Z) {
-                        if (app.strokes.count > 0) {
-                            app.strokes.strokes[app.strokes.count - 1].hidden = true;
-                            app.strokes.count--;
-                        }
+                        undo_stroke(&app);
                     } else if (event.key.key == SDLK_Y) {
-                        if (app.strokes.strokes[app.strokes.count].hidden) {
-                            app.strokes.strokes[app.strokes.count].hidden = false;
-                            app.strokes.count++;
-                        }
+                        redo_stroke(&app);
                     }
                 }
             }
@@ -115,11 +139,13 @@ int main(int argc, char *argv[])
         // TODO: draw corners
 
         SDL_SetRenderDrawColor(app.renderer, 0, 0, 0, 255);
-        SDL_RenderFillRect(app.renderer, &clear.rect);
+        for (int i = 0; buttons[i].text; ++i)
+            SDL_RenderFillRect(app.renderer, &buttons[i].rect);
 
         SDL_SetRenderDrawColor(app.renderer, 255, 0, 0, 255);
 
-        SDL_RenderTexture(app.renderer, clear.texture, NULL, &clear.rect);
+        for (int i = 0; buttons[i].text; ++i)
+            SDL_RenderTexture(app.renderer, buttons[i].texture, NULL, &buttons[i].rect);
 
         for (int i = 0; i < app.strokes.count; ++i)
             if (!app.strokes.strokes[i].hidden)
